e41f2141776c0f7181b04561366c3d6c062db121
C Daly <colin.p.s.daly@gmail.com>
2020-08-02 19:34:15 +0100

    Added Routes to App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ProfileComponent } from '../app/components/profile/profile.component';
+import { LandingComponent } from '../app/components/landing/landing.component';
+import { PortfolioComponent } from '../app/components/portfolio/portfolio.component';
+import { ContactComponent } from '../app/components/contact/contact.component';
+
+const routes: Routes = [
+  { path: 'home', component: LandingComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact' , component: ContactComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
